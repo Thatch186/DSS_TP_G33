5c6f87640ae889914b79d55c9206204da465dd2f
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2021-12-26 22:33:43 +0000

    Equipamento FIX

--- src/Equipamento.java
+++ src/Equipamento.java
@@ -2,10 +2,25 @@ public class Equipamento {
     private String codRegisto;
     private boolean reparado;
 
+    /*
+    CONSTRUCTORS
+     */
     public Equipamento(String cod){
         this.codRegisto =cod;
         reparado=false;
     }
+    public Equipamento(String cod, boolean reparado){
+        this.codRegisto = cod;
+        this.reparado = reparado;
+    }
+    public Equipamento(Equipamento outro){
+        this.codRegisto = outro.getCodRegisto();
+        this.reparado = outro.isReparado();
+    }
+    
+    /*
+    GETTERS e SETTERS
+     */
 
     public String getCodRegisto() {
         return codRegisto;
@@ -23,6 +38,30 @@ public class Equipamento {
         this.reparado = reparado;
     }
 
+    /*
+    EQUALS
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        Equipamento e = (Equipamento) o;
+        return ((this.reparado == e.isReparado()) &&
+                this.codRegisto.equals(e.getCodRegisto()));
+    }
+    /*
+    CLONE
+     */
+    public Equipamento clone(){
+        return new Equipamento(this);
+    }
+    /*
+    TO STRING
+     */
     @Override
     public String toString() {
         return "Equipamento(" +
