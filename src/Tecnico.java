5a7018c6e2447cddf45260061b1eefb492901463
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2021-12-26 16:10:21 +0000

    Tecnico

--- src/Tecnico.java
+++ src/Tecnico.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Tecnico {
@@ -6,5 +7,75 @@ public class Tecnico {
     private List<String> idExpressos;
     private List<String> idRealizados;
 
+    /*
+     CONSTUCTORS
+     */
+    public Tecnico(String id, List<String> ipr, List<String> ie, List<String> ir){
+        this.id = id;
+        this.idPorRealizar = new ArrayList<>(ipr);
+        this.idExpressos = new ArrayList<>(ie);
+        this.idRealizados = new ArrayList<>(ir);
+    }
+    public Tecnico(Tecnico outro){
+        this.id = outro.getId();
+        this.idPorRealizar = outro.getIdPorRealizar();
+        this.idExpressos = outro.getIdExpressos();
+        this.idRealizados = outro.getIdRealizados();
+    }
 
+    /*
+    GETTERS e SETTERS
+     */
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public List<String> getIdPorRealizar() {
+        return new ArrayList<>(this.idPorRealizar);
+    }
+    public void setIdPorRealizar(List<String> idPorRealizar) {
+        this.idPorRealizar = new ArrayList<>(idPorRealizar);
+    }
+
+    public List<String> getIdExpressos() {
+        return new ArrayList<>(idExpressos);
+    }
+
+    public void setIdExpressos(List<String> idExpressos) {
+        this.idExpressos = new ArrayList<>(idExpressos);
+    }
+
+    public List<String> getIdRealizados() {
+        return new ArrayList<>(idRealizados);
+    }
+    public void setIdRealizados(List<String> idRealizados) {
+        this.idRealizados = new ArrayList<>(idRealizados);
+    }
+
+    /*
+    EQUALS
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        Tecnico tec = (Tecnico) o;
+        return (this.id.equals(tec.getId()) &&
+                this.idRealizados.equals(tec.getIdRealizados()) &&
+                this.idExpressos.equals(tec.getIdExpressos()) &&
+                this.idPorRealizar.equals(tec.getIdPorRealizar()));
+    }
+    /*
+    CLONE
+     */
+    @Override
+    public Tecnico clone(){
+        return new Tecnico(this);
+    }
 }
