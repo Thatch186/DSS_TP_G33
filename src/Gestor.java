ae4ed823c460e4a1b6fcb2784bc96b1c5b9003d5
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2021-12-26 17:19:04 +0000

    Gestor

--- src/Gestor.java
+++ src/Gestor.java
@@ -1,11 +1,43 @@
 public class Gestor {
     private String id;
 
+    /*
+    CONTRUCTORS
+     */
     public Gestor(String id){
         this.id=id;
     }
-
-    String getId(){
+    public Gestor(Gestor o){
+        this.id = o.getId();
+    }
+    /*
+    GETTERS e SETTERS
+     */
+    public String getId(){
         return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+
+    /*
+    EQUALS
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        Gestor g = (Gestor) o;
+        return (this.id.equals(g.getId()));
+    }
+    /*
+    CLONE
+     */
+    public Gestor clone(){
+        return new Gestor(this);
     }
 }
