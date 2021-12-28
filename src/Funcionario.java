b0733594bfbb93beb05fc1d7cd044a125197583e
juliogoncalves147 <juliobeites@hotmail.com>
2021-12-28 15:36:36 +0000

    password

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,3 +1,4 @@
+import java.lang.constant.Constable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -5,19 +6,22 @@ import java.util.Objects;
 
 public class Funcionario {
     private String id;
+    private String password;
     private List<String> idAtendimentos; //Ids de Equipamentos
     private List<String> idEntregas; //Ids de Equipamentos
 
     /*
     Constructors
      */
-    public Funcionario(String id){
+    public Funcionario(String id, String password){
         this.id=id;
+        this.password = password;
         idAtendimentos = new ArrayList<>();
         idEntregas = new ArrayList<>();
     }
-    public Funcionario(String id, List<String> idAtendimentos, List<String> idEntregas){
+    public Funcionario(String id, String password, List<String> idAtendimentos, List<String> idEntregas){
         this.id = id;
+        this.password = password;
         this.idAtendimentos = new ArrayList<>(idAtendimentos);
         this.idEntregas = new ArrayList<>(idEntregas);
     }
@@ -25,11 +29,22 @@ public class Funcionario {
         this.id = o.getId();
         this.idAtendimentos = o.getIdAtendimentos();
         this.idEntregas = o.getIdEntregas();
+        this.password = o.getPassword();
     }
+
+
 
     /*
     GETTERS e SETTERS
      */
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     String getId(){
         return id;
     }
@@ -83,7 +98,8 @@ public class Funcionario {
         Funcionario f = (Funcionario) o;
         return (this.id.equals(f.getId()) &&
                 this.idEntregas.equals(f.getIdEntregas()) &&
-                this.idAtendimentos.equals(f.getIdAtendimentos()));
+                this.idAtendimentos.equals(f.getIdAtendimentos()) &&
+                this.password.equals(f.getPassword()));
     }
     /*
     CLONE
